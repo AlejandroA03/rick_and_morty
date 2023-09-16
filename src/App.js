fd352e8c70e61e19f1390437be4d42ea09c92971
AlejandroA03 <a-a-m2001@gmail.com>
2023-09-16 17:21:10 -0500

    Homework 08

--- src/App.js
+++ src/App.js
@@ -1,17 +1,63 @@
 import styled from 'styled-components';
-import Card from './components/Card.jsx';
 import Cards from './components/Cards.jsx';
-import SearchBar from './components/SearchBar.jsx';
-import characters, { Rick } from './data.js';
+import Nav from './components/Nav.jsx';
+import { useState } from 'react';
+import axios from 'axios';
 
 const PrincipalDiv = styled.div`
-   background-color: #8ca0ff`
+   `
 
 function App() {
+
+   const [characters, setCharacters] = useState([])
+
+   const onSearch = (id) => {
+      axios(`https://rickandmortyapi.com/api/character/${id}`).then(
+        ({ data }) => {
+          if (data.name) {
+            const isCharacterAdded = characters.some(
+              (char) => char.id === data.id
+            );
+            if (isCharacterAdded) {
+              window.alert("¡El personaje ya ha sido agregado!");
+            } else {
+              setCharacters((oldChars) => [...oldChars, data]);
+            }
+          } else {
+            window.alert("¡No hay personajes con este ID!");
+          }
+        }
+      );
+    };
+  
+    const addRandomCharacter = () => {
+      axios("https://rickandmortyapi.com/api/character").then(({ data }) => {
+        const randomCharacter =
+          data.results[Math.floor(Math.random() * data.results.length)];
+        const isCharacterAdded = characters.some(
+          (char) => char.id === randomCharacter.id
+        );
+        if (isCharacterAdded) {
+          addRandomCharacter();
+        } else {
+          setCharacters((oldChars) => [...oldChars, randomCharacter]);
+        }
+      });
+    };
+  
+
+   const onClose= (id) =>{
+      setCharacters(
+         characters.filter((char)=>{
+            return char.id !== Number(id)
+         })
+      )
+   }
+
    return (
       <PrincipalDiv>
-         <SearchBar onSearch={(characterID) => window.alert(characterID)} />
-         <Cards characters={characters} />
+         <Nav onSearch={onSearch} addRandomCharacter={addRandomCharacter}/>
+         <Cards characters={characters} onClose={onClose} />
       </PrincipalDiv>
    );
 }
